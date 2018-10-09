dba53e19498e5e7b68090ea386fcc79af1f4846b
Liam <lccoombs@hotmail.co.uk>
2018-10-09 09:38:20 +0000

    notes_app.rb --> create_note_prompt method started and finished!

--- lib/notes_app.rb
+++ lib/notes_app.rb
@@ -5,8 +5,17 @@ class Notes_app
   def initialize
     @stored_notes = Array.new 
   end
+  
   def add_note(title, body)
     note = Note.new(title, body)
     @stored_notes.push(note)
+  end
+  
+  def create_note_prompt
+    puts "Title of your note:"
+    title = gets.chomp
+    puts "And the body?"
+    body = gets.chomp
+    add_note(title, body)
   end
 end
--- spec/notes_app_spec.rb
+++ spec/notes_app_spec.rb
@@ -2,7 +2,7 @@ require 'notes_app'
 require 'note'
 
 describe Notes_app do
-  describe '#add_node' do
+  describe '#add_node' do #this method is now private so tests have been removed
     it 'responds to #add_note' do
       expect(subject).to respond_to(:add_note)
     end
