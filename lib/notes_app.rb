be8aeab2553876f546e21bcae1a381bee71f72e8
Liam <lccoombs@hotmail.co.uk>
2018-10-09 09:13:02 +0000

    notes_app.rb --> add_note method finished

--- lib/notes_app.rb
+++ lib/notes_app.rb
@@ -1,10 +1,12 @@
 require_relative 'note'
 
 class Notes_app
-    def intialize
-       @stored_notes = Array.new 
-    end
-    def add_note
-        
-    end
+  private :add_note
+  def intialize
+    @stored_notes = Array.new 
+  end
+  def add_note(title, body)
+    note = Note.new(title, body)
+    @stored_notes.push(note)
+  end
 end
